3fce254dd854660a1b2523730e51cf9b9ee016d9
Fabian Enos <fabian.enos@gmail.com>
2019-01-24 18:44:17 +0530

    Added support and tests for different types of joinTypes in the $lookup stage. Added nyc for test coverage.

--- test/lookup.js
+++ test/lookup.js
@@ -190,11 +190,181 @@ describe('$lookup tests', function() {
                     _id: "$districtName",
                     count: {
                         "$count": 1
-                    }, 
+                    } 
                 }
             }
         ]);
 
         assert.equal(result, "SELECT `districtName` as `_id`, COUNT(*) as `count` FROM (SELECT `states`.`name` as `stateName`, `t0`.`districtName`, `t0`.`uuid`, `t0`.`email` FROM (SELECT `districts`.`name` as `districtName`, `districts`.`state_id` as `state_id`, `users`.`uuid`, `users`.`email` FROM `users` LEFT OUTER JOIN `districts` ON `districts`.`uuid` = `users`.`district_id`) t0 LEFT OUTER JOIN `states` ON `states`.`uuid` = `t0`.`state_id`) t1 GROUP BY `districtName`");
+    });
+
+    it('should run a left outer join by default', function() {
+        let result = mongoToSQL.$lookup({
+            from: "states",
+            foreignField: "id",
+            localField: "state_id",
+            as: {
+                "state_id": 1,
+                "stateName": "$name",
+                "dummy": "dummy",
+                "boolean": true,
+                "number": 10.5
+            }
+        }, "users");
+
+        assert.equal(result.success, true);
+        assert.equal(result.query, "SELECT `users`.`state_id`, `states`.`name` as `stateName`, 'dummy' as `dummy`, true as `boolean`, 10.5 as `number` FROM `users` LEFT OUTER JOIN `states` ON `states`.`id` = `users`.`state_id`");
+    });
+
+    it('should run a left outer join by configuration', function() {
+        let result = mongoToSQL.$lookup({
+            from: "states",
+            foreignField: "id",
+            localField: "state_id",
+            joinType: "left",
+            as: {
+                "state_id": 1,
+                "stateName": "$name",
+                "dummy": "dummy",
+                "boolean": true,
+                "number": 10.5
+            }
+        }, "users");
+
+        assert.equal(result.success, true);
+        assert.equal(result.query, "SELECT `users`.`state_id`, `states`.`name` as `stateName`, 'dummy' as `dummy`, true as `boolean`, 10.5 as `number` FROM `users` LEFT OUTER JOIN `states` ON `states`.`id` = `users`.`state_id`");
+    });
+
+    it('should run a right outer join by configuration', function() {
+        let result = mongoToSQL.$lookup({
+            from: "states",
+            foreignField: "id",
+            localField: "state_id",
+            joinType: "right",
+            as: {
+                "state_id": 1,
+                "stateName": "$name",
+                "dummy": "dummy",
+                "boolean": true,
+                "number": 10.5
+            }
+        }, "users");
+
+        assert.equal(result.success, true);
+        assert.equal(result.query, "SELECT `users`.`state_id`, `states`.`name` as `stateName`, 'dummy' as `dummy`, true as `boolean`, 10.5 as `number` FROM `users` RIGHT OUTER JOIN `states` ON `states`.`id` = `users`.`state_id`");
+    });
+
+    it('should run a full outer join by configuration', function() {
+        let result = mongoToSQL.$lookup({
+            from: "states",
+            foreignField: "id",
+            localField: "state_id",
+            joinType: "full",
+            as: {
+                "state_id": 1,
+                "stateName": "$name",
+                "dummy": "dummy",
+                "boolean": true,
+                "number": 10.5
+            }
+        }, "users");
+
+        assert.equal(result.success, true);
+        assert.equal(result.query, "SELECT `users`.`state_id`, `states`.`name` as `stateName`, 'dummy' as `dummy`, true as `boolean`, 10.5 as `number` FROM `users` FULL OUTER JOIN `states` ON `states`.`id` = `users`.`state_id`");
+    });
+
+    it('should run an inner join by configuration', function() {
+        let result = mongoToSQL.$lookup({
+            from: "states",
+            foreignField: "id",
+            localField: "state_id",
+            joinType: "inner",
+            as: {
+                "state_id": 1,
+                "stateName": "$name",
+                "dummy": "dummy",
+                "boolean": true,
+                "number": 10.5
+            }
+        }, "users");
+
+        assert.equal(result.success, true);
+        assert.equal(result.query, "SELECT `users`.`state_id`, `states`.`name` as `stateName`, 'dummy' as `dummy`, true as `boolean`, 10.5 as `number` FROM `users` INNER JOIN `states` ON `states`.`id` = `users`.`state_id`");
+    });
+
+    it('should run fail as the joinType is invalid', function() {
+        let result = mongoToSQL.$lookup({
+            from: "states",
+            foreignField: "id",
+            localField: "state_id",
+            joinType: "inner1",
+            as: {
+                "state_id": 1,
+                "stateName": "$name",
+                "dummy": "dummy",
+                "boolean": true,
+                "number": 10.5
+            }
+        }, "users");
+
+        assert.equal(result.success, false);
+        assert.equal(result.error, mongoToSQL.Errors.INVALID_JOIN_TYPE("inner1"));
+    });
+
+    it('should default to left outer join when the joinType is a number', function() {
+        let result = mongoToSQL.$lookup({
+            from: "states",
+            foreignField: "id",
+            localField: "state_id",
+            joinType: 1,
+            as: {
+                "state_id": 1,
+                "stateName": "$name",
+                "dummy": "dummy",
+                "boolean": true,
+                "number": 10.5
+            }
+        }, "users");
+
+        assert.equal(result.success, true);
+        assert.equal(result.query, "SELECT `users`.`state_id`, `states`.`name` as `stateName`, 'dummy' as `dummy`, true as `boolean`, 10.5 as `number` FROM `users` LEFT OUTER JOIN `states` ON `states`.`id` = `users`.`state_id`");
+    });
+
+    it('should default to left outer join when the joinType is a boolean', function() {
+        let result = mongoToSQL.$lookup({
+            from: "states",
+            foreignField: "id",
+            localField: "state_id",
+            joinType: false,
+            as: {
+                "state_id": 1,
+                "stateName": "$name",
+                "dummy": "dummy",
+                "boolean": true,
+                "number": 10.5
+            }
+        }, "users");
+
+        assert.equal(result.success, true);
+        assert.equal(result.query, "SELECT `users`.`state_id`, `states`.`name` as `stateName`, 'dummy' as `dummy`, true as `boolean`, 10.5 as `number` FROM `users` LEFT OUTER JOIN `states` ON `states`.`id` = `users`.`state_id`");
+    });
+
+    it('should default to left outer join when the joinType is null', function() {
+        let result = mongoToSQL.$lookup({
+            from: "states",
+            foreignField: "id",
+            localField: "state_id",
+            joinType: null,
+            as: {
+                "state_id": 1,
+                "stateName": "$name",
+                "dummy": "dummy",
+                "boolean": true,
+                "number": 10.5
+            }
+        }, "users");
+
+        assert.equal(result.success, true);
+        assert.equal(result.query, "SELECT `users`.`state_id`, `states`.`name` as `stateName`, 'dummy' as `dummy`, true as `boolean`, 10.5 as `number` FROM `users` LEFT OUTER JOIN `states` ON `states`.`id` = `users`.`state_id`");
     });
 });
