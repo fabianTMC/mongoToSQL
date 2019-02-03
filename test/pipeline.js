e21d2d2685d5679d759f016f3a63061eceaf1932
Fabian Enos <fabian.enos@gmail.com>
2019-02-03 16:10:14 +0530

    Added  operator for ordering of query results

--- test/pipeline.js
+++ test/pipeline.js
@@ -70,4 +70,219 @@ describe('mixed pipeline tests', function() {
         
         assert.equal(result, "SELECT `status` FROM (SELECT * FROM (SELECT * FROM `inventory` WHERE `status` = 'A') t0 WHERE `status` = 'D') t1 WHERE `qty` = 2");
     });
+
+    it('optimize sql query based on $match before $project at a later secondary stage with an order', function() {
+        let result = mongoToSQL.convert(resource, [
+            {
+                "$match": {
+                    "status": "A",
+                }
+            },
+            {
+                "$match": {
+                    "status": "D",
+                }
+            },
+            {
+                "$match": {
+                    "qty": 2
+                }
+            },
+            {
+                "$project": {
+                    "status": 1
+                }
+            },
+            {
+                "$order": [
+                    {
+                        "status": 1
+                    }
+                ]
+            }
+            
+        ]);
+        assert.equal(result, "SELECT `status` FROM (SELECT * FROM (SELECT * FROM `inventory` WHERE `status` = 'A') t0 WHERE `status` = 'D') t1 WHERE `qty` = 2 ORDER BY `status` ASC");
+    });
+
+    it('optimize sql query based on $match before $project at a later secondary stage with an order and then follow up with a $match', function() {
+        let result = mongoToSQL.convert(resource, [
+            {
+                "$match": {
+                    "status": "A",
+                }
+            },
+            {
+                "$match": {
+                    "status": "D",
+                }
+            },
+            {
+                "$match": {
+                    "qty": 2
+                }
+            },
+            {
+                "$project": {
+                    "status": 1
+                }
+            },
+            {
+                "$order": [
+                    {
+                        "status": 1
+                    }
+                ]
+            },
+            {
+                "$match": {
+                    "status": 10
+                }
+            }
+            
+        ]);
+        assert.equal(result, "SELECT * FROM (SELECT `status` FROM (SELECT * FROM (SELECT * FROM `inventory` WHERE `status` = 'A') t0 WHERE `status` = 'D') t1 WHERE `qty` = 2 ORDER BY `status` ASC) t4 WHERE `status` = 10");
+    });
+
+    it('optimize sql query based on $match before $project at a later secondary stage with multiple orders and then follow up with a $match', function() {
+        let result = mongoToSQL.convert(resource, [
+            {
+                "$match": {
+                    "status": "A",
+                }
+            },
+            {
+                "$match": {
+                    "status": "D",
+                }
+            },
+            {
+                "$match": {
+                    "qty": 2
+                }
+            },
+            {
+                "$project": {
+                    "status": 1
+                }
+            },
+            {
+                "$order": [
+                    {
+                        "status": 1
+                    },
+                    {
+                        "qty": -1
+                    }
+                ]
+            },
+            {
+                "$match": {
+                    "status": 10
+                }
+            }
+            
+        ]);
+        assert.equal(result, "SELECT * FROM (SELECT `status` FROM (SELECT * FROM (SELECT * FROM `inventory` WHERE `status` = 'A') t0 WHERE `status` = 'D') t1 WHERE `qty` = 2 ORDER BY `status` ASC, `qty` DESC) t4 WHERE `status` = 10");
+    });
+
+    it('should error because of invalid $order', function() {
+        let result = mongoToSQL.convert(resource, [
+            {
+                "$project": {
+                    "status": 1
+                }
+            },
+            {
+                "$order": [
+                    
+                ]
+            }
+        ]);
+
+        assert.equal(result.success, false);
+        assert.equal(result.error, mongoToSQL.Errors.EMPTY_ORDER);
+    });
+
+    it('should error because of invalid $order', function() {
+        let result = mongoToSQL.convert(resource, [
+            {
+                "$project": {
+                    "status": 1
+                }
+            },
+            {
+                "$order": [
+                    {
+                        "status": 2
+                    }
+                ]
+            }
+        ]);
+
+        assert.equal(result.success, false);
+        assert.equal(result.error, mongoToSQL.Errors.INVALID_ORDER);
+    });
+
+    it('should error because of invalid $order in the second order', function() {
+        let result = mongoToSQL.convert(resource, [
+            {
+                "$project": {
+                    "status": 1
+                }
+            },
+            {
+                "$order": [
+                    {
+                        "id": 1,
+                    },
+                    {
+                        "status": 2
+                    }
+                ]
+            }
+        ]);
+
+        assert.equal(result.success, false);
+        assert.equal(result.error, mongoToSQL.Errors.INVALID_ORDER);
+    });
+
+    it('should error because of invalid $order object', function() {
+        let result = mongoToSQL.convert(resource, [
+            {
+                "$project": {
+                    "status": 1
+                }
+            },
+            {
+                "$order": [
+                    ["status"]
+                ]
+            }
+        ]);
+
+        assert.equal(result.success, false);
+        assert.equal(result.error, mongoToSQL.Errors.INVALID_ORDER_OBJECT);
+    });
+
+    it('should error because of invalid second $order object', function() {
+        let result = mongoToSQL.convert(resource, [
+            {
+                "$project": {
+                    "status": 1
+                }
+            },
+            {
+                "$order": [
+                    {
+                        "id": 1,
+                    },
+                    ["status"]
+                ]
+            }
+        ]);
+
+        assert.equal(result.success, false);
+        assert.equal(result.error, mongoToSQL.Errors.INVALID_ORDER_OBJECT);
+    });
 })
